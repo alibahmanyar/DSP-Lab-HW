8ee6992d34d5cfeee33ca99771e661c9a0a2ad8e
Ali Bahmanyar <abbahmanyar@gmail.com>
2023-10-10 20:56:50 +0330

    Implement part 1.3 Refactor some variables

--- Exp 1/q1.m
+++ Exp 1/q1.m
@@ -25,21 +25,30 @@ grid on;
 
 
 % part 1.2
-t = 0:0.01:2; % Define time axis
-A = 5;
-f = 1;
-
-y1 = A*sin(2*pi*f*t); % Normal sine
-
-r = rand(1, length(y1)) - 0.5; % Noise
-y2 = y1 + r; % Add noise to sine
+r = rand(1, length(y)) - 0.5; % Noise
+noised_signal = y + r; % Add noise to sine
 
 % Plotting the signals
 figure('Name', 'Normal Sine signal VS Noisy Sine Signal');
 subplot(2,1,1);
-plot(t,y1)
+plot(t,y)
 title('Normal Signal');xlabel('Time (Second)');ylabel('Signal Amplitude');
 subplot(2,1,2);
-plot(t,y2)
+plot(t,noised_signal)
 title('Noisy Signal');xlabel('Time (Second)');ylabel('Signal Amplitude');
+hold on;
+
+% part 1.3:
+M1 = 0;
+M2 = 20;
+len = M2 - M1 + 1; % Window length
+
+window = ones(1, len) / len; % Moving average window
+
+y2 = conv(noised_signal, window);
+t2 = 0:0.01:(0.01 * (length(y2) - 1));
+
+plot(t2, y2);
+
+
 
