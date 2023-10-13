642b1cec832af98a047be3baed39baec4190d126
cavendishsama <alibabaloo.aut@gmail.com>
2023-10-13 11:31:55 +0330

    minor changes have done regarding 1st and 2nd part

--- Exp 1/q1.m
+++ Exp 1/q1.m
@@ -1,11 +1,12 @@
+%%
 close all;
 clear;
 clc;
-
+%%
 % part 1.1
-t = 0:0.01:2; % Define time axis
-A = 5;
-f = 1;
+t = 0:0.01:2;        % Define time axis
+A = 5;               % Amplitude
+f = 1;               % Frequency
 y = A*sin(2*pi*f*t); % Sine function
 
 % Plotting signals
@@ -17,27 +18,34 @@ title('Sine signal');
 grid on;
 
 figure('Name', 'Sine signal (Stem)');
-stem(t, y, 'LineWidth', 0.5);
+stem(t, y,'color', '#7E2F8E', 'LineWidth', 0.5);
 xlabel('Time (s)');
 ylabel('Signal Amplitude');
 title('Sine signal');
 grid on;
 
-
+%%
 % part 1.2
 r = rand(1, length(y)) - 0.5; % Noise
-noised_signal = y + r; % Add noise to sine
+noised_signal = y + r;        % Add noise to sine, y defined in section 1.1
 
 % Plotting signals
 figure('Name', 'Normal Sine signal VS Noisy Sine Signal');
-subplot(2,1,1);
+subplot(3,1,1);               % Plotting normal signal without noise
 plot(t,y)
 title('Normal Signal');xlabel('Time (Second)');ylabel('Signal Amplitude');
-subplot(2,1,2);
+subplot(3,1,2);               % Plotting noisy signal
 plot(t,noised_signal)
 title('Noisy Signal');xlabel('Time (Second)');ylabel('Signal Amplitude');
+%hold on;
+subplot(3,1,3);               % Plotting both signal together for comparing them
+plot(t,y, t, noised_signal)
 hold on;
+title('Comparing signals');xlabel('Time (Second)');ylabel('Signal Amplitude');
+legend('Normal signal', 'Noisy signal');
+hold off;
 
+%%
 % part 1.3:
 M1 = 0;
 M2 = 20;
@@ -50,7 +58,6 @@ t2 = 0:0.01:(0.01 * (length(y2) - 1));
 
 plot(t2, y2);
 
-
 % part 1.3.2:
 M1 = 10;
 M2 = 10;
@@ -62,8 +69,8 @@ y3 = conv(noised_signal(M1:end), window);
 t3 = 0:0.01:(0.01 * (length(y3) - 1));
 
 plot(t3, y3);
-
 legend("Noisy Signal", "Moving Avg M1=0, M2=20", "Moving Avg M1=10, M2=10");
 
-
+%%
+% part 1.4
 
