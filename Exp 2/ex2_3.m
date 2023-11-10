8b86c0871aa0ae3ca16c2a5d427d1e1df6c8a878
cavendishsama <alibabaloo.aut@gmail.com>
2023-11-10 15:09:08 +0330

    DWT, wavelet part added and plotted

--- Exp 2/ex2_3.m
+++ Exp 2/ex2_3.m
@@ -14,3 +14,20 @@ grid on; title('Noisy Doppler');
 
 [cA1,cD1] = dwt(x_noisy,'db1');
 loc = linspace(0,1,length(cA1));
+
+figure('Name', '3 Levels');
+subplot(3,1,1);
+plot(loc,cA1, 'r', 'LineWidth',1.5);
+grid on; title('Level 1');
+
+[cA2,cD2] = dwt(cA1,'db1');
+loc = linspace(0,1,length(cA2));
+subplot(3,1,2);
+plot(loc,cA2, 'm', 'LineWidth',1.5);
+grid on; title('Level 2');
+
+[cA3,cD3] = dwt(cA2,'db1');
+loc = linspace(0,1,length(cA3));
+subplot(3,1,3);
+plot(loc,cA3, 'g', 'LineWidth',1.5);
+grid on; title('Level 3');
