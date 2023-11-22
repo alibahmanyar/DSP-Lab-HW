1cb9f1c31b7720ff4c435df1ac9eeecdfe50b490
Ali <abbahmanyar@gmail.com>
2023-11-22 14:32:51 +0330

    Minor change

--- Exp 3/m2.m
+++ Exp 3/m2.m
@@ -1,21 +1,11 @@
 close all;
 clear;
 clc;
-
-%% 
+%%
 N = 10e4;
-f = [4, 8 ,12];
 n = 0:N;
 
-fs = 400;
-
-t1 = 0:(1/fs):(2-1/fs);
-t2 = 2:(1/fs):(4-1/fs);
-t3 = 4:(1/fs):(6-1/fs);
-x = [cos(2*pi*f(1)*t1),cos(2*pi*f(2)*t2),cos(2*pi*f(3)*t3)];
-
-
-
+%% 3.2.b
 b1 = [0.969, -1.92377, 0.969531];
 a1 = [1, -1.923772, 0.9399063];
 
@@ -52,3 +42,48 @@ grid on;
 settling_time_value = find(abs(y_step - y_step(end)) >= 0.01, 1, 'last') + 1;
 
 disp(sprintf("Setteling sample: %d", settling_time_value));
+
+
+%% 3.2.c
+f = [4, 8 ,12];
+fs = 400;
+
+t1 = 0:(1/fs):(2-1/fs);
+t2 = 2:(1/fs):(4-1/fs);
+t3 = 4:(1/fs):(6-1/fs);
+x = [cos(2*pi*f(1)*t1),cos(2*pi*f(2)*t2),cos(2*pi*f(3)*t3)];
+
+figure('Name', 'x[tn]');
+stem(x, "LineWidth", 1.5);
+title("x[tn]");
+xlabel("n");
+ylabel("Amplitude");
+grid on;
+hold on;
+
+y1 = filter(b1, a1, x);
+
+% figure('Name', 'x[tn]');
+stem(y1, "LineWidth", 1.5);
+% title("x[tn]");
+% xlabel("n");
+% ylabel("Amplitude");
+% grid on;
+hold off;
+
+%%%%% H2:
+figure('Name', 'x[tn]');
+stem(x, "LineWidth", 1.5);
+title("x[tn]");
+xlabel("n");
+ylabel("Amplitude");
+grid on;
+hold on;
+y2 = filter(b2, a2, x);
+
+% figure('Name', 'H2');
+stem(y2, "LineWidth", 1.5);
+% title("x[tn]");
+% xlabel("n");
+% ylabel("Amplitude");
+% grid on;
