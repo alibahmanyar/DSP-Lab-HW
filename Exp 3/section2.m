6993063094223ac879e025c372ef89564194b494
Ali Bahmanyar <abbahmanyar@gmail.com>
2023-12-03 11:21:48 +0330

    Implement 3.2.f

--- Exp 3/section2.m
+++ Exp 3/section2.m
@@ -2,11 +2,7 @@ close all;
 clear;
 clc;
 
-%% 3-2-a
-close all;
-clear;
-clc;
-
+%%
 % H1:
 b1 = [0.969531, -1.923772, 0.969531];
 a1 = [1, -1.923772, 0.939063];
@@ -14,7 +10,7 @@ a1 = [1, -1.923772, 0.939063];
 % H2:
 b2 = [0.996088, -1.976468, 0.996088];
 a2 = [1, -1.976468, 0.992177];
-
+%% 3-2-a
 % H1:
 figure('name', 'H1 response')
 freqz(b1,a1,1024,'whole');
@@ -94,3 +90,47 @@ y2 = filter(b2, a2, x);
 plot(y2, "LineWidth", 1.5);
 hold off;
 legend('x[tn]', 'y[tn]')
+
+%% 3.2.d
+N = 300;
+n = 0:N;
+
+impr1 = impz(b1,a1,n);
+impr2 = impz(b2,a2,n);
+
+[max_h1, indx] = max(impr1);
+
+[max_h2, indx] = max(impr2);
+fprintf("Max H1: %f\nMax H2: %f\n", max_h1, max_h2)
+
+figure('name', "Impulse Res H1")
+plot(n, abs(H1), 'LineWidth', 1.5);
+grid on;
+xlabel('time');
+ylabel('|H1(z)|');
+title('Impulse Response H1');
+
+
+figure('name', "Impulse Res H2")
+plot(n, abs(H2), 'LineWidth', 1.5);
+grid on;
+xlabel('time');
+ylabel('|H2(z)|');
+title('Impulse Response H2');
+
+%% 3.2.f
+fs  = 400;
+[h1, w1] = freqz(b1,a1,2048);
+[h2, w2] = freqz(b2,a2,2048);
+
+% Plotting frequency response of H1 and H2
+figure('name', "Notch Filter Responses")
+plot(w1/pi*fs/2, abs(h1), 'LineWidth', 1.5);
+xlim([0, 20]);
+grid on;
+xlabel('freq'); 
+ylabel('|H(f)|'); 
+hold on;
+title('Frequency Responses of H1 and H2');
+plot(w2/pi*fs/2, abs(h2), '--', 'LineWidth', 1.5);
+legend('H1(f) Δf = 4', 'H2(f) Δf = 0.5', 'Location','best');
