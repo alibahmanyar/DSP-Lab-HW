d13bd7f0339c2ba9805e07a6f72c2929c8390ae7
cavendishsama <alibabaloo.aut@gmail.com>
2023-12-06 15:14:00 +0330

    Filtered are designed by FilterDesigner Toolbox, part h is complete

--- Exp 4/exp4.m
+++ Exp 4/exp4.m
@@ -78,3 +78,43 @@ title('Original Image');
 subplot(1,2,2)
 imshow(salt_papper_noisy);
 title('Noisy Image');
+%% 4.2.6
+kernel = ones(3,3) / 9;
+filtered_salt_pepper = imfilter(salt_papper_noisy,kernel);
+
+figure('name', "Filtered Image");
+subplot(1,3,1)
+imshow(img2);
+title('Original Image');
+subplot(1,3,2)
+imshow(salt_papper_noisy);
+title('Noisy Image');
+subplot(1,3,3)
+imshow(filtered_salt_pepper);
+title('Filtered Image');
+%% 4.2.7
+load('filter.mat');
+filter_FIR = ftrans2(Num);
+figure('name', "Filtered Image")
+subplot(2,1,1)
+freqz(Num);
+subplot(2,1,2)
+freqz2(filter_FIR);
+
+%% 4.2.8
+FIR_gaussian = imfilter(noisy, filter_FIR);
+FIR_salt_pepper = imfilter(salt_papper_noisy, filter_FIR);
+
+figure('name', "Filtered Images")
+subplot(2,2,1)
+imshow(noisy);
+title('Noisy Image');
+subplot(2,2,2)
+imshow(FIR_gaussian);
+title('FIR Gaussian filter');
+subplot(2,2,3)
+imshow(salt_papper_noisy);
+title('Salt & Pepper Noise Image');
+subplot(2,2,4)
+imshow(FIR_salt_pepper);
+title('FIR salt & pepper filter');
